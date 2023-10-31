0136ca7bff74d80a933db7b22caa309a6a061b2c
Scriabinian <148218052+Scriabinian@users.noreply.github.com>
2023-10-31 21:49:09 +0900

    몽킥킥킥
    콕킼깈기

--- 연습ㅎㅎ.java
+++ 연습ㅎㅎ.java
@@ -1,88 +1,45 @@
 package day4;
 import java.util.*;
-
+/*
 public class 연습ㅎㅎ {
 	public static void main(String[] args) {
-		/*int num = 456;
-		System.out.println((num+"").charAt(0)+"00");
 	
-		*/
-		
-		/*
-		int fahrenheit = 100;
-		float celcius =((int)(5/9.0*(fahrenheit-32)*100+0.5))/100.0f;
-		System.out.println("Fahrenheit:"+fahrenheit);
-		System.out.println("celcius:"+celcius);
-		*/
-		
-		/*
-		String s="";
-		for(int i=1;i<=5;i++) {
-			System.out.println(i);
-			s += i+"";
-		}
-		
-		System.out.println(s);*/
-		/*
-		for(int i=0;i<5;i++) {
-			for(int j=0;j<=i;j++) {
-				System.out.print("*");
-			}
-			System.out.println("");
-		}
-		 */
-		/*
-		for(int i=5;i>=1;i--) {
-			for(int j=i;j>=1;j--) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-		*/
-		/*
-		for(int i=1;i<=5;i++) {
-			for(int j=5;j-i>=0;j--) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-		*/
-		/*
 		연습ㅎㅎ i = new 연습ㅎㅎ();
-		int n = i.sum(4);
+		int n = i.sum(10);
 		System.out.println(n);
 	}
 
 
 int sum(int n) {//1,4,10,20,35,56;
-	 
-	if(n==0) {
-		return 0;
-	}
-	if(n==1) {
-		return 1;
-	}else{
-		return sum(n-1)*2-sum(n-2)+n;
-	}
-}*/
-		/*
-		int target =  (int)(Math.random()*100)+1;
-		do {
-			Scanner s = new Scanner(System.in);
-			int t = s.nextInt();
-				
-			int i=t>target ? -1:t<target?1:0;
-			if(i==0) {
-				System.out.println("right.");
-				break;
-			}
-			else if(i<0)
-				System.out.println("down.");
-			else
-				System.out.println("up");
-		}while(true);
-		*/
-		
-		
+		if(n==0) {
+			return 0;
+		}
+		if(n==1) {
+			return 1;
+		}else{
+			return sum(n-1)*2-sum(n-2)+n;
+		}
 	}
 }
+
+*/
+public class 연습ㅎㅎ{
+	public static void main(String[] args) {
+		/*
+		int[] arrs = new int[5];
+		for (int i=0; i<arrs.length; i++) {
+			arrs[i] = i;
+		}
+		
+		int[] arr2 = arrs;
+		
+		Arrays.toString(배열);
+		 
+		System.arraycopy(arrs, 0, arr2, 0, 배열크기);
+		arr2 = Arrays.copyOf(arrs, arrs.length);
+
+	
+		*/
+	}
+}
+
